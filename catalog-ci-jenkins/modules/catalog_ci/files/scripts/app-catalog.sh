cc522d377c8ff058f028dac6cac093e75334c538
Sergey Kolekonov <skolekonov@mirantis.com>
2015-05-25 15:54:26 +0300

    Update scripts for Jenkins jobs

--- catalog-ci-jenkins/modules/catalog_ci/files/scripts/app-catalog.sh
+++ catalog-ci-jenkins/modules/catalog_ci/files/scripts/app-catalog.sh
@@ -25,6 +25,10 @@ upload_image () {
     CONFIG=$TMP_DIR/$(basename $IMAGE_CONFIG)
     OLD_CONFIG=$TMP_DIR/$(basename $IMAGE_CONFIG).old
     IMAGE_NAME=$(python $WDIR/generate_names.py glance $OLD_CONFIG $CONFIG)
+    if [ -z "$IMAGE_NAME" ]; then
+      echo "Image file can't be generated"
+      exit 1
+    fi
 
     echo "Uploading image $IMAGE_NAME from $IMAGE_PATH"
     mv $IMAGE_PATH $(dirname $IMAGE_PATH)/$IMAGE_NAME
@@ -44,11 +48,15 @@ main() {
     HASH=$(cat $TMP_FILE | egrep "^\s*Image-hash:\s[A-Za-z0-9]*$" | egrep -o "[A-Za-z0-9]*$")
 
     cat $TMP_FILE | grep  Image-URL | grep -q Unknown && exit 0
+#    if [ $(cat $TMP_FILE | grep Image-URL | grep Unknown) ]; then
+#      echo "Image URL is unknown, no checks and changes will be performed"
+#      exit 0
+#    fi
 
-    if [ -z $URL -o -z $HASH ]; then
+    if [ -z "$URL" -o -z "$HASH" ]; then
       echo "Image URL or hash wasn't found"
       clean
-      exit 1
+      exit 0
     else
       HTTP_CODE=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $URL)
       if ! [[ " ${CODES[*]} " == *" $HTTP_CODE "* ]]; then
@@ -58,11 +66,10 @@ main() {
       fi
     fi
 
-
+    wget $URL -P $TMP_DIR
     if [ "$HASH" == "Unknown" ]; then
       echo "Image hash is unknown, skipping checks..."
     else
-      wget $URL -P $TMP_DIR
       REAL_HASH=$(md5sum $TMP_DIR/* | awk '{print $1}')
       if [ "$REAL_HASH" != "$HASH" ]; then
         echo "Hash mismatch"
@@ -76,8 +83,7 @@ main() {
     if [ "$EVENT" == "change-merged" ]; then
       IMAGE=$(ls $TMP_DIR/*)
       cp $IMAGE_CONFIG $TMP_DIR
-      git checkout $GERRIT_BRANCH
-      git pull
+      git checkout HEAD~1
       cp $IMAGE_CONFIG $TMP_DIR/$(basename $IMAGE_CONFIG).old
       upload_image $IMAGE
    else
--- catalog-ci-jenkins/modules/catalog_ci/files/scripts/generate_names.py
+++ catalog-ci-jenkins/modules/catalog_ci/files/scripts/generate_names.py
@@ -15,27 +15,26 @@ def yaml_to_dict(infile, k):
 def diff_images_config(images1, images2):
     if images1 == images2:
         return
+    import itertools
+    import re
+    intersec = [item for item in images1 if item in images2]
+    sym_diff = [item for item in itertools.chain(images1,images2) if item not in intersec]
+    name = ''
+    d_size = len(sym_diff)
+    if d_size <= 2:
+      i = d_size -1
+    else:
+      return ''
 
-    ddiff = DeepDiff(images1, images2)
-    type = ddiff.changes.keys()[0]
-
-    if type == 'values_changed':
-        changes = ddiff.changes['values_changed']
-        tmp = re.search('"root\[\d\]', str(changes)).group(0)
-        res_index = re.search('\d', tmp).group(0)
-        res = images2[int(res_index)]
-        print res['name'].replace(' ', '-').lower() + '.' + res['format'].lower()
-    elif type == 'list_added':
-        changes = ddiff.changes['list_added'][0]
-        tmp = changes[0]
-        image_name = re.search(
-           '\'name\':\s\'[A-z0-9-_.\s]*\'', changes).group(0).split(' ')[1][1:-1]
-        image_format = re.search(
-            '\'format\':\s\'[A-z0-9]*\'', changes).group(0).split(' ')[1][1:-1]
-        print image_name.replace(' ', '-').lower + '.' + image_format.lower()
+    if 'name' in sym_diff[i].keys() and 'format' in sym_diff[i].keys():
+       i_name  = re.sub('[(){}<>]', '', sym_diff[i]['name'])
+       i_type  = sym_diff[i]['format']
+       name = i_name + '.' + i_type
+       name = name.lower().replace (" ", "_")
+    return name
 
 if __name__ == '__main__':
     if argv[1] == 'glance':
         images1 = yaml_to_dict(argv[2], 'images')
         images2 = yaml_to_dict(argv[3], 'images')
-        diff_images_config(images1, images2)
+        print(diff_images_config(images1, images2))
